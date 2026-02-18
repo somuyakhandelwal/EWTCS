74a76479717c707edad0c6fdd62708050bf1c7e6
Avni mujawdiya <avnimujawdiya008@gmail.com>
2026-02-18 17:57:28 +0530

    chore: remove temporary db fix scripts

--- make-admin.mjs
+++
@@ -1,9 +0,0 @@
-import pg from 'pg'
-import dotenv from 'dotenv'
-dotenv.config({ path: '.env.local' })
-
-const pool = new pg.Pool({ connectionString: process.env.DATABASE_URL })
-
-await pool.query(`UPDATE users SET role = 'admin' WHERE username = 'nurse'`)
-console.log('Admin role restored!')
-await pool.end()
